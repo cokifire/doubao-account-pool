1d5be3ce5114f7c1e0b48dff0acfe35506ad0f3c
yaonieyo <108744540+yaonieyo@users.noreply.github.com>
2026-08-06 12:05:45 +0800

    fix: show current version before API startup

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ type TabKey = "accounts" | "settings" | "logs";
 const accounts = ref<Account[]>([]);
 const apiRequests = ref<ApiRequest[]>([]);
 const apiStatus = ref<ApiServerStatus>({
-  version: "0.1.18",
+  version: "0.1.24",
   enabled: false,
   running: false,
   port: 0,
